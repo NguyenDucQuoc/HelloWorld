4ad36b71336c20ef304e614ab98afd3e8f21db13
NguyenDucQuoc <88954732+NguyenDucQuoc@users.noreply.github.com>
2021-10-04 14:07:50 +0700

    Update MainApp.java

--- src/quoc/dev/MainApp.java
+++ src/quoc/dev/MainApp.java
@@ -4,6 +4,16 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String firstName= "Phan Van";
+		String lastName= "Nam";
+		intold= 15;floatmarkMath= 9f;
+		booleanpassed= true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName+ " "+ lastName);
+		System.out.println("Mark Math: "+ markMath);
+		System.out.println("Passed: "+ passed);
+		//Khaibáo hằng. Hằng phải viết hoa
+		finalfloatPI= 3.14f;
 
 	}
 
